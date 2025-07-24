18730e641ede500392a918f77c3b59ea96959fd5
Ed <edwarddavis@hotmail.co.uk>
2025-07-24 17:25:12 +0100

    implemented rest of the transformer

--- gpt.py
+++ gpt.py
@@ -14,12 +14,16 @@ batch_size = 32  # how many independent sequences will we process in parallel?
 block_size = 8  # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
-learning_rate = 1e-2
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+learning_rate = 1e-3
+epochs = 10000
+epoch_checkpoints = epochs / 10
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "cpu"
 eval_iters = 200
 n_dim = 32
 head_size = 32
+num_heads = 4
+dropout = 0.2
 # %%
 # Load input text data
 with open("input.txt", "r", encoding="utf-8") as f:
@@ -69,6 +73,7 @@ class attnHead(nn.Module):
         self.Q = nn.Linear(n_dim, head_size, bias=False)
         self.K = nn.Linear(n_dim, head_size, bias=False)
         self.V = nn.Linear(n_dim, head_size, bias=False)
+        self.dropout = nn.Dropout(dropout)
 
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
 
@@ -83,14 +88,73 @@ class attnHead(nn.Module):
         attn_mat /= C**0.5
 
         # Causal masked attention + softmax
+        # Note the [:T, :T] as the input size is variable (number of tokens in the current block)
+        # NOTE: this makes this a decoder block
         attn_mat = attn_mat.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
         attn_mat = F.softmax(
             attn_mat, dim=-1
         )  # (B, T, T), dim=-1 normalises across rows
 
+        attn_mat = self.dropout(attn_mat)
+
         logits = attn_mat @ v  # (B, T, T) @ (B, T, C) -> (B, T, C)
 
         return logits  # (B, T, C)
+
+
+class MultiHeadAttn(nn.Module):
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.attn_heads = nn.ModuleList([attnHead(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_dim, n_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        all_attn_heads = [head(x) for head in self.attn_heads]  # (B, T, C) * num_heads
+        x = torch.cat(
+            all_attn_heads, dim=-1
+        )  # concat over channel dim; (B, T, C*num_heads)
+        x = self.proj(x)
+        logits = self.dropout(x)
+        return logits
+
+
+class FeedForward(nn.Module):
+
+    def __init__(self, n_dim):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(n_dim, n_dim * 4),
+            nn.ReLU(),
+            nn.Linear(n_dim * 4, n_dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    """Transformer block"""
+
+    def __init__(self, n_dim, num_heads):
+        super().__init__()
+
+        head_size = n_dim // num_heads
+        self.mh_attn = MultiHeadAttn(num_heads, head_size)
+        self.ln1 = nn.LayerNorm(n_dim)
+        self.ffwd = FeedForward(n_dim)
+        self.ln2 = nn.LayerNorm(n_dim)
+
+    def forward(self, x):
+        # Skip connections to help train with a deep network
+        x = self.ln1(x)
+        x = x + self.mh_attn(x)
+        x = self.ln2(x)
+        x = x + self.ffwd(x)
+        return x
 
 
 class LanguageModel(nn.Module):
@@ -101,8 +165,15 @@ class LanguageModel(nn.Module):
 
         # Embedding to track the position of the token within the context window
         self.position_embedding = nn.Embedding(block_size, n_dim)
+
+        self.transformer = nn.Sequential(
+            Block(n_dim, num_heads),
+            Block(n_dim, num_heads),
+            Block(n_dim, num_heads),
+            Block(n_dim, num_heads),
+            nn.LayerNorm(n_dim),
+        )
         self.lm_head = nn.Linear(n_dim, vocab_size)
-        self.attn_head = attnHead(head_size=head_size)
 
     def forward(self, idx):
         B, T = idx.shape  # batch_size, context_length
@@ -110,10 +181,10 @@ class LanguageModel(nn.Module):
         tok_emb = self.token_embedding(idx)  # batch_size, context_length, n_dim
         pos_emb = self.position_embedding(torch.arange(T, device=device))
         emb = tok_emb + pos_emb
-        emb = self.attn_head(emb)  # (B, T, n_dim)
-        logits = self.lm_head(emb)  # batch_size, context_length, vocab_size  (B, T, C)
+        x = self.transformer(emb)  # (B, T, n_dim)
+        logits = self.lm_head(x)  # batch_size, context_length, vocab_size  (B, T, C)
 
-        return logits
+        return logits  # (B, T, C)
 
     def generate(self, n_tokens):
         # Start from a zero token
@@ -170,18 +241,17 @@ def estimate_loss(model, eval_iters=200):
 model = LanguageModel().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
-epochs = 1000
 
 print(decoder(model.generate(n_tokens=100)[0].tolist()))
 
 # %%
 train_losses = []
 test_losses = []
-epoch_checkpoints = 100
 for epoch in range(epochs):
 
     if epoch % epoch_checkpoints == 0:
         losses = estimate_loss(model)
+        print(f"Epoch {epoch}")
         print(f"Train loss: {losses['train']}\nTest loss: {losses['test']}")
         train_losses.append(losses["train"])
         test_losses.append(losses["test"])
@@ -209,12 +279,12 @@ _ = plt.grid(alpha=0.2)
 _ = plt.plot(train_losses, marker="o", label="Train")
 _ = plt.plot(test_losses, marker="o", label="Test")
 _ = plt.legend()
-_ = plt.xlabel(f"Per {epoch_checkpoints} Epochs")
+_ = plt.xlabel(f"{int(epoch_checkpoints)} Epochs")
 _ = plt.ylabel("Loss")
 
 
 # %%
-print(decoder(model.generate(n_tokens=100)[0].tolist()))
+print(decoder(model.generate(n_tokens=1000)[0].tolist()))
 
 
 # %%
